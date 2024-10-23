07bf9a61c8a617ff905029814587f63763efec27
jidutianxia <jdtx1244664354@163.com>
2024-10-23 20:16:51 +0800

    Site updated: 2024-10-23 20:16:50

--- js/runtime.js
+++ js/runtime.js
@@ -1,1 +1,36 @@
-setInterval((()=>{let e=Math.round(new Date("2024-10-07 00:00:00").getTime()/1e3),t=Math.round((new Date).getTime()/1e3)-e,r=new Array(0,0,0,0,0);var i=function(e){return e>9?e:"0"+e};t>=31536e3&&(r[0]=parseInt(t/31536e3),t%=31536e3),t>=86400&&(r[1]=parseInt(t/86400),t%=86400),t>=3600&&(r[2]=i(parseInt(t/3600)),t%=3600),t>=60&&(r[3]=i(parseInt(t/60)),t%=60),t>0&&(r[4]=i(t)),Number(r[2])<22&&Number(r[2])>7?currentTimeHtml="<img class='boardsign' src='https://img.shields.io/badge/MC-green?style=color=green&logo=minetest' title='距离百年老店也就差不到一百年~'><div id='runtime'>"+r[0]+" YEAR "+r[1]+" DAYS "+r[2]+" : "+r[3]+" : "+r[4]+"</div>":currentTimeHtml="<img class='boardsign' src='https://img.shields.io/badge/打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>"+r[0]+" YEAR "+r[1]+" DAYS "+r[2]+" : "+r[3]+" : "+r[4]+"</div>",document.getElementById("workboard").innerHTML=currentTimeHtml}),1e3);
+setInterval(() => {
+    let create_time = Math.round(new Date('2024-10-07 00:00:00').getTime() / 1000); //在此行修改建站时间
+    let timestamp = Math.round((new Date().getTime()) / 1000);
+    let second = timestamp - create_time;
+    let time = new Array(0, 0, 0, 0, 0);
+  
+    var nol = function(h){
+      return h>9?h:'0'+h;
+    }
+    if (second >= 365 * 24 * 3600) {
+      time[0] = parseInt(second / (365 * 24 * 3600));
+      second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {
+      time[1] = parseInt(second / (24 * 3600));
+      second %= 24 * 3600;
+    }
+    if (second >= 3600) {
+      time[2] = nol(parseInt(second / 3600));
+      second %= 3600;
+    }
+    if (second >= 60) {
+      time[3] = nol(parseInt(second / 60));
+      second %= 60;
+    }
+    if (second > 0) {
+      time[4] = nol(second);
+    }
+    if ((Number(time[2])<22) && (Number(time[2])>7)){
+      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/MC-%E4%B8%8A%E7%8F%AD%E6%91%B8%E9%B1%BC~-green?style=color=social&logo=minetest' title='此生不悔入MC,来世仍做方块人'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    }
+    else{
+      currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/打烊了-6adea8?style=social&logo=coffeescript' title='这个点了应该去睡觉啦，熬夜对身体不好哦'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+    }
+    document.getElementById("workboard").innerHTML = currentTimeHtml;
+  }, 1000);
